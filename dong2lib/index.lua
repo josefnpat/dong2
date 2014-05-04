3f17098eeeb271417ed82ab7ba8486cc2f2c0af9
Josef Patoprsty <seppi@josefnpat.com>
2014-05-04 18:09:09 -0400

    Condensing the ouya PS3 discrepency and adding comments

--- dong2lib/index.lua
+++ dong2lib/index.lua
@@ -1,12 +1,9 @@
 local index = {
   XBOX_360 = "Xbox 360 Wireless Receiver",
   OUYA = "OUYA Game Controller",
+  PS3 = love._os == "Android" and
+    "PLAYSTATION(R)3 Controller" or -- OUYA
+    "Sony PLAYSTATION(R)3 Controller" -- EVERYONE ELSE
 }
-
-if love._os == "Android" then
-  index["PS3"] = "PLAYSTATION(R)3 Controller"
-else
-  index["PS3"] = "Sony PLAYSTATION(R)3 Controller"
-end
 
 return index
